fcb7b9da62bbd5c8ac72b8be6cdc18a2b07eac6f
StelIify <34519030+StelIify@users.noreply.github.com>
2020-06-21 19:56:05 +0300

    Update AbstractFactory.java

--- Programming patterns. Factory, Builder/AbstractFactory.java
+++ Programming patterns. Factory, Builder/AbstractFactory.java
@@ -1,21 +1,22 @@
-/*
- * Rhombus
- *
- * Programming patterns. Factory, Builder
- *
- * Copyright Oleksandr Zorenko KNUTE
- *
- * Develop for your class
- *
- * 1. Factory.
- * 2. Abstract factory.
- * 3. Create a class Student  - 25 fields.
- * 4. Create a builder for the class Student
- */
+/**
+ * @author Oleksandr Zorenko KNUTE
+ * @version 1.0
+ * Classname AbstractFactory
+ * Class represents the programming pattern 'Abstract factory' for geometric figure Rhombus
+ *  Module 2 Task 4. Programming patterns. Factory, Builder.
+ *  Task description: Develop Abstract factory for your class
+**/
 package com.company;
 
 public class AbstractFactory
 {
+    /**
+     * Class constructor with parameters
+     * Creates a class instance depending on requested figure
+     * @param a and b set a sides of a Rhombus
+     * @param figure specifies which figure should be created
+     **/
+    
     public  static IGeometry create(int a, int b, Figure figure)
     {
         switch (figure)
